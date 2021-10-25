e7e0fac5df9435b66044793898344f5a5cf27102
SeHuibixiaoxing <wangzeyujiuyi@qq.com>
2021-10-25 16:53:41 +0800

    cow

--- kernel/spinlock.h
+++ kernel/spinlock.h
@@ -1,9 +1,16 @@
 // Mutual exclusion lock.
-struct spinlock {
-  uint locked;       // Is the lock held?
+#include "memlayout.h"
+
+struct spinlock
+{
+  uint locked; // Is the lock held?
 
   // For debugging:
-  char *name;        // Name of lock.
-  struct cpu *cpu;   // The cpu holding the lock.
+  char *name;      // Name of lock.
+  struct cpu *cpu; // The cpu holding the lock.
 };
-
+struct q
+{
+  struct spinlock lock;
+  int memref[PHYSTOP / 4096];
+};
